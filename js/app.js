26fa566b0cf1a3ea873b391ec03f91e2786cea45
Sam Richard <snugug@gmail.com>
2014-03-19 16:27:05 -0400

    Two-Way Data Binding, Import/Export

--- js/app.js
+++ js/app.js
@@ -1,13 +1,78 @@
-(function (angular, localStorage) {
+(function (angular) {
   'use strict';
 
-  var intakeApp = angular.module('intakeApp', []);
+  var intakeApp = angular.module('intakeApp', ['LocalStorageModule']);
 
   if (!String.prototype.contains) {
     String.prototype.contains = function () {
       return String.prototype.indexOf.apply(this, arguments) !== -1;
     };
   }
+
+  if (!String.prototype.slugify) {
+    String.prototype.slugify = function () {
+      return this.toLowerCase().replace(/[^\w ]+/g, '').replace(/ +/g, '-');
+    };
+  }
+
+  intakeApp.factory('dataService', function (localStorageService) {
+    var client = localStorageService.get('client') || {};
+    return {
+      getClient: function () {
+        return client;
+      },
+      setClient: function (value) {
+        client = value;
+      }
+    };
+  });
+
+  intakeApp.directive('import', function (dataService, localStorageService) {
+    return {
+      restrict: 'E',
+      template: 'Import: <input type="file" name="import">',
+      link: function (scope, elem) {
+        elem.bind('change', function (e) {
+          var file = e.target.files[0];
+          var reader = new FileReader();
+
+          reader.onloadend = function (evt) {
+            var importData = JSON.parse(evt.target.result);
+
+            localStorageService.add('client', importData.client);
+            location.reload(true);
+          };
+
+          reader.readAsText(file);
+        });
+      }
+    };
+  });
+
+  intakeApp.controller('IntakeHeader', function ($scope, dataService) {
+    $scope.client = dataService.getClient();
+    console.log();
+
+    $scope.IntakeDownload = function () {
+      var bundle = document.createElement('a');
+      var prepare = {'client': $scope.client};
+      bundle.href = window.URL.createObjectURL(new Blob([JSON.stringify(prepare)], { type: 'text/plain'}));
+      bundle.download = $scope.client.name.slugify() + '--intake.txt';
+
+      document.body.appendChild(bundle);
+      bundle.click();
+      document.body.removeChild(bundle);
+    };
+  });
+
+  intakeApp.controller('IntakeClientName', function ($scope, dataService, localStorageService) {
+    $scope.client = dataService.getClient();
+
+    // Save Client info when client.name changes
+    $scope.$watch('client.name', function () {
+      localStorageService.add('client', $scope.client);
+    });
+  });
 
   intakeApp.filter('schemaTypeFilter', function () {
     return function (input, filterKey) {
@@ -33,7 +98,7 @@
     };
   });
 
-  intakeApp.controller('IntakeContentModelCtrl', function ($scope, $http) {
+  intakeApp.controller('IntakeContentModelCtrl', function ($scope, $http, localStorageService) {
 
     function setScope(schema) {
       $scope.dataTypes = schema.datatypes;
@@ -41,34 +106,17 @@
       $scope.types = schema.types;
     }
 
-    var schemaAll = localStorage.getItem('schemaAll');
-    if (!schemaAll) {
+    var schema = localStorageService.get('schema');
+    if (!schema) {
       $http.get('data/all.json').success(function (schema) {
-        localStorage.setItem('schemaAll', JSON.stringify(schema));
+        localStorageService.add('schema', schema);
         setScope(schema);
       });
     }
     else {
-      setScope(JSON.parse(schemaAll));
+      setScope(schema);
     }
-
-
-
-
-    $scope.models = [
-      {'name': 'TV Episode',
-        'description': 'A TV episode which can be part of a series or season.',
-        'machine': 'TVEpisode'},
-      {'name': 'TV Series',
-        'description': 'Series dedicated to TV broadcast and associated online delivery.',
-        'machine': 'TVSeries'},
-      {'name': 'TV Season',
-        'description': 'Season dedicated to TV broadcast and associated online delivery.',
-        'machine': 'TVSeason'}
-    ];
-
-    $scope.orderProp = 'age';
   });
 
 
-})(window.angular, window.localStorage);
+})(window.angular);
