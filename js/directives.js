e3b00b39647d84e530bc35a914a37ddeb288be20
Sam Richard <snugug@gmail.com>
2014-04-02 15:05:10 -0400

    Added Uploaded state

--- js/directives.js
+++ js/directives.js
@@ -56,6 +56,7 @@
         var dropZone = document.getElementById('drop_zone');
         if (scope.image) {
           dropZone.style.backgroundImage = 'url(' + scope.image + ')';
+          dropZone.setAttribute('data-state', 'uploaded');
         }
         var opts = {
           readAsMap: {
